114bbeee56dcacc814a852d8b508524d52d536cf
Roman Yefimets <romanyefimets@yahoo.com>
2018-04-12 17:13:24 -0700

    first commit

---
+++ Assets/scripts/characterController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour {
+
+    [SerializeField]
+    Rigidbody2D rigidBody;
+
+    [SerializeField]
+    Animator animator;
+
+    [SerializeField]
+    float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+
+
+    }
+    void Update ()
+    {
+        animator.SetFloat("speed", rigidBody.velocity.magnitude);
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (moveHorizontal < 0)
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        else
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rigidBody.AddForce(movement * speed);
+
+    }
+}
