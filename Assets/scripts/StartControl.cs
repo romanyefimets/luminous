186f1794424bc6ed72319fa04a2a44b333a7b3a8
a305 <32826964+a305@users.noreply.github.com>
2018-04-24 12:49:57 -0700

    Added Start Menu

---
+++ Assets/scripts/StartControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartControl : MonoBehaviour {
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene("main");
+    }
+}
