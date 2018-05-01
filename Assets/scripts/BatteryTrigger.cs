f31ae9753504b9186bb18f8b44a80edb2b136309
a305 <32826964+a305@users.noreply.github.com>
2018-05-01 13:35:51 -0700

    Updated Stuff for sprint 2

---
+++ Assets/scripts/BatteryTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryTrigger : MonoBehaviour {
+
+    public float increaseAmount = 30;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // On collision with player, increase player's batterylife by given amount and destroy this object
+        if (other.tag == "Player")
+        {
+            other.transform.Find("armPivot").Find("arm").GetComponent<FlashLightControl>().SetBattery(increaseAmount);
+            Destroy(gameObject);
+        }
+    }
+}
