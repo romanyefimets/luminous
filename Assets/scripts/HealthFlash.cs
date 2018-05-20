68f6374d75521147c52e0d73fa150bcc166dedc1
Roman Yefimets <romanyefimets@yahoo.com>
2018-05-20 14:56:02 -0700

    added red screen blink

---
+++ Assets/scripts/HealthFlash.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor.AnimatedValues;
+
+public class HealthFlash : MonoBehaviour {
+
+    Image image; // assign your logo here
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        image = GetComponent<Image>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+    }
+
+    public void flash()
+    {
+        StartCoroutine(flashInput(image));
+    }
+
+    public IEnumerator flashInput(Image input)
+    {
+        print("test");
+        Color imageColor = image.color;
+        imageColor.a = .3f;
+        image.color = imageColor;
+        yield return new WaitForSeconds(.2f);
+        imageColor.a = 0;
+        image.color = imageColor;
+        
+    }
+
+}
