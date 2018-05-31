5e750675268f878b342be046536c6fffa1ccc134
Roman Yefimets <romanyefimets@yahoo.com>
2018-05-30 22:57:21 -0700

    video edits

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -6,12 +6,13 @@ public class CameraController : MonoBehaviour {
 
     public GameObject player;       //Public variable to store a reference to the player game object
     private Vector3 offset;         //Private variable to store the offset distance between the player and camera
-
+    private bool track;
     // Use this for initialization
     void Start()
     {
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
         offset = transform.position - player.transform.position;
+        track = true;
     }
 
     private void Update()
@@ -20,6 +21,20 @@ public class CameraController : MonoBehaviour {
         {
             exit();
         }
+        if (Input.GetButton("PanLeft"))
+        {
+            gameObject.transform.position += new Vector3(-.05f, 0, 0);
+        }
+        else if (Input.GetButton("PanRight"))
+        {
+            gameObject.transform.position += new Vector3(.05f, 0, 0);
+        }
+
+        if (Input.GetButtonDown("Track"))
+        {
+            track = !track;
+        }
+        
     }
 
     public void exit()
@@ -31,7 +46,7 @@ public class CameraController : MonoBehaviour {
     void LateUpdate()
     {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        if (player != null)
+        if (player != null && track)
             transform.position = player.transform.position + offset;
     }
 }
